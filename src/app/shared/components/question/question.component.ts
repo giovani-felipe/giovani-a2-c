bd99e4685bd0f5e88f3892d31b667aa756baaade
giovani-felipe <37030674+giovani-felipe@users.noreply.github.com>
2023-06-03 12:47:45 +0100

    1 file modified, 1 added, 1 deleted

--- src/app/shared/components/question/question.component.ts
+++ src/app/shared/components/question/question.component.ts
@@ -6,7 +6,7 @@ import { Question } from '../../../models/question';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss'],
+  styleUrls: ['./question.component.css'],
   imports: [NgFor, NgIf, NgClass],
   standalone: true,
 })
